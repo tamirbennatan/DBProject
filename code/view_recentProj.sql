783213c690054bfa1e56a9628225469afbf66d25
Tamir Bennatan <timibennatan@gmail.com>
2018-02-27 16:21:43 -0500

    Delete view_recentProj.sql
    I don't think this is a useful view. It will select all the projects. The same as "SELECT * FROM project*.

--- code/view_recentProj.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW recent_project AS 
-   SELECT proj_id
-   FROM project 
-   WHERE date_created > '2017-01-01';
-
-   
-/* Recent_project represents the tasks that are the most recent (after 2017-01-01), 
-we select them based on their date_created, those that were created after 2017-01-01 
-are treated as recent. */
