82766de5fee2bcd6d62c96c66abd7a57d30bf721
Feixuan Chi(Pauline) <chifeixuan02@126.com>
2018-03-01 18:26:36 -0500

    update q7

--- code/views/view_devMostPoints.sql
+++ code/views/view_devMostPoints.sql
@@ -4,17 +4,38 @@
 -- script of what happens when try run SQL UPDATE statement of each vie
 -- summarize con that must hold so DB2/postgreSQL allows updating a view
 
-CREATE view devMostPoints AS 
-        SELECT 
-        ticketdeveloperassignment.devid, lowticket.pointestimate      
-        FROM 
-                lowticket, ticketdeveloperassignment
-                WHERE lowticket.tid = ticketdeveloperassignment.tid 
-                ORDER BY pointestimate DESC;
+/*
+* From this devMostPoints table we can get a list of developers,  
+* we sort them based on the points they get in descending order. 
+* From this table it's easy for the team and company 
+* to distinguish which developers did more jobs. 
 
-/* LeastUrgentTask represent the tasks that are of least urgency, 
-we select them based on their urgency value, and group those that are 
-less than 2 to be the least urgent tasks. 
+Relevent information extracted:
+ * 
+ * - developer id
+ * - the point
+ * - team name (which scheduled the ticket)
+ * - business owner which partitioned ticket from a high level ticket
+ * - high level ticket id from which each ticket was partitioned
+ * - point estimate of low level ticket.
+
+*/
+
+
+create view devMostPoints as 
+    select devid, sum(pointestimate) FROM 
+        (select 
+               ticketDeveloperassignment.devid, lowticket.pointestimate
+               from 
+                        lowticket, ticketdeveloperassignment
+                        where lowticket.tid = ticketdeveloperassignment.tid
+                        order by pointestimate desc) a 
+     group by devid order by sum(pointestimate) desc;
+
+
+
+
+/* 
 Views are read only, the system does not change according to an update on a
 view. We can get the effect of an updatable view by creating INSTEAD triggers 
 on the view, which must convert attempted inserts, etc. on the view into 
